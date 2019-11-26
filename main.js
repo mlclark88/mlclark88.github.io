c038b5025319801294e603f8e5955adc0b667c5b
mlclark88 <35897842+mlclark88@users.noreply.github.com>
2019-11-25 23:52:36 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+//index- main page
+$(document).ready(function() {
+    $('.header').height($(window).height());
+
+})
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous buttons
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// thumbnails
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+//faq dropdown menu
+$('.dropdown-toggle').dropdown();
